62d5025053cca625c377078572af4005271b6117
paulobonifacio <paulo.bonifacio@live.com>
2025-03-15 18:38:20 +0100

    update

--- src/js/product.js
+++ src/js/product.js
@@ -1,14 +1,13 @@
-import { getParam } from './utils.mjs';
-import ProductData from './ProductData.mjs';
-import ProductDetails from './ProductDetails.mjs';
+import { getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 // Get product Id from URL
-const productId = getParam('product');
+const productId = getParam("product");
 
 // Create an instance of ProductData to fetch the data
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
 // Create an instance of ProductDetails and start loading
 const product = new ProductDetails(productId, dataSource);
 product.init();
-
